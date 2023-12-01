a08cbb283380f370c10ad1836c21ff9214b6a688
mike-dydx <mike@dydx.exchange>
2023-12-01 16:38:38 -0500

    refactor fees card with reusable title card view

--- dydx/dydxViews/dydxViews/_v4/Profile/Components/dydxProfileFeesView.swift
+++ dydx/dydxViews/dydxViews/_v4/Profile/Components/dydxProfileFeesView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import PlatformUI
 import Utilities
 
-public class dydxProfileFeesViewModel: PlatformViewModel, Equatable {
+public class dydxProfileFeesViewModel: dydxTitledCardViewModel, Equatable {
     public static func == (lhs: dydxProfileFeesViewModel, rhs: dydxProfileFeesViewModel) -> Bool {
         lhs.tradingVolume == rhs.tradingVolume &&
         lhs.takerFeeRate == rhs.takerFeeRate &&
@@ -20,11 +20,12 @@ public class dydxProfileFeesViewModel: PlatformViewModel, Equatable {
     @Published public var tradingVolume: String?
     @Published public var takerFeeRate: String?
     @Published public var makerFeeRate: String?
-    @Published public var tapAction: (() -> Void)?
 
-    public init() { }
+    public init() {
+        super.init(title: DataLocalizer.localize(path: "APP.GENERAL.FEES"))
+    }
 
-    public static var previewValue: dydxProfileFeesViewModel {
+    fileprivate static var previewValue: dydxProfileFeesViewModel {
         let vm = dydxProfileFeesViewModel()
         vm.tradingVolume = "$120,000"
         vm.takerFeeRate = "0.01%"
@@ -32,76 +33,52 @@ public class dydxProfileFeesViewModel: PlatformViewModel, Equatable {
         return vm
     }
 
-    public override func createView(parentStyle: ThemeStyle = ThemeStyle.defaultStyle, styleKey: String? = nil) -> PlatformView {
-        PlatformView(viewModel: self, parentStyle: parentStyle, styleKey: styleKey) { [weak self] style in
-            guard let self = self else { return AnyView(PlatformView.nilView) }
-
-            let view = VStack(spacing: 0) {
+    public override var content: AnyView {
+        AnyView(
+            VStack(spacing: 16) {
                 HStack {
-                    Text(DataLocalizer.localize(path: "APP.GENERAL.FEES"))
-                        .themeFont(fontSize: .small)
-                    Spacer()
-                    PlatformIconViewModel(type: .system(name: "chevron.right"),
-                                          size: CGSize(width: 10, height: 10),
-                                          templateColor: .textSecondary)
-                    .createView(parentStyle: style)
-                }
-                .padding()
+                    VStack(spacing: 8) {
+                        Text(DataLocalizer.localize(path: "APP.TRADE.TAKER"))
+                            .themeFont(fontType: .text, fontSize: .smaller)
+                            .leftAligned()
 
-                DividerModel()
-                    .createView(parentStyle: style)
-
-                VStack(spacing: 16) {
-                    HStack {
-                        VStack(spacing: 8) {
-                            Text(DataLocalizer.localize(path: "APP.TRADE.TAKER"))
-                                .themeFont(fontType: .text, fontSize: .smaller)
-                                .leftAligned()
-
-                            Text(self.takerFeeRate ?? "-")
-                                .themeFont(fontType: .text, fontSize: .small)
-                                .themeColor(foreground: .textPrimary)
-                                .leftAligned()
-                        }
-
-                        VStack(spacing: 8) {
-                            Text(DataLocalizer.localize(path: "APP.TRADE.MAKER"))
-                                .themeFont(fontType: .text, fontSize: .smaller)
-                                .leftAligned()
-
-                            Text(self.makerFeeRate ?? "-")
-                                .themeFont(fontType: .text, fontSize: .small)
-                                .themeColor(foreground: .textPrimary)
-                                .leftAligned()
-                        }
+                        Text(self.takerFeeRate ?? "-")
+                            .themeFont(fontType: .text, fontSize: .small)
+                            .themeColor(foreground: .textPrimary)
+                            .leftAligned()
                     }
 
                     VStack(spacing: 8) {
-                        HStack {
-                            Text(DataLocalizer.localize(path: "APP.TRADE.VOLUME"))
+                        Text(DataLocalizer.localize(path: "APP.TRADE.MAKER"))
+                            .themeFont(fontType: .text, fontSize: .smaller)
+                            .leftAligned()
 
-                            Text(DataLocalizer.localize(path: "APP.GENERAL.TIME_STRINGS.30D"))
-                                .themeColor(foreground: .textTertiary)
-                        }
-                        .themeFont(fontType: .text, fontSize: .smaller)
-                        .leftAligned()
-
-                        Text(self.tradingVolume ?? "-")
+                        Text(self.makerFeeRate ?? "-")
                             .themeFont(fontType: .text, fontSize: .small)
                             .themeColor(foreground: .textPrimary)
                             .leftAligned()
                     }
                 }
-                .padding(16)
-            }
-            .themeColor(background: .layer3)
-            .cornerRadius(12, corners: .allCorners)
-            .onTapGesture { [weak self] in
-                self?.tapAction?()
-            }
 
-            return AnyView(view)
-        }
+                VStack(spacing: 8) {
+                    HStack {
+                        Text(DataLocalizer.localize(path: "APP.TRADE.VOLUME"))
+
+                        Text(DataLocalizer.localize(path: "APP.GENERAL.TIME_STRINGS.30D"))
+                            .themeColor(foreground: .textTertiary)
+                    }
+                    .themeFont(fontType: .text, fontSize: .smaller)
+                    .leftAligned()
+
+                    Text(self.tradingVolume ?? "-")
+                        .themeFont(fontType: .text, fontSize: .small)
+                        .themeColor(foreground: .textPrimary)
+                        .leftAligned()
+                }
+            }
+            .padding(16)
+
+        )
     }
 }
 
