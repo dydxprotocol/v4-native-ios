1883d25e13613e9c83d3f6a5890e5cb738f5e07b
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2023-12-04 16:58:54 -0500

    Mike/trcl 3314 trading rewards profile card (#36)
    * refactor fees card with reusable title card view
    
    * refactor history view to use dydxTitledCardView
    
    * change name for profile "rewards" -> "balances" card
    
    * do not wrap "reduce-only" text
    
    * add rewards card to profile landing screen
    
    * add comment
    
    * reorder cards
    
    * clean up
    
    * set up presenter view model links

---
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/Components/dydxProfileBalancesViewPresenter.swift
@@ -0,0 +1,59 @@
+//
+//  dydxProfileBalancesViewPresenter.swift
+//  dydxPresenters
+//
+//  Created by Rui Huang on 9/18/23.
+//
+
+import Utilities
+import dydxViews
+import PlatformParticles
+import RoutingKit
+import ParticlesKit
+import PlatformUI
+import Abacus
+import dydxStateManager
+import dydxFormatter
+import Combine
+
+public protocol dydxProfileBalancesViewPresenterProtocol: HostedViewPresenterProtocol {
+    var viewModel: dydxProfileBalancesViewModel? { get }
+}
+
+public class dydxProfileBalancesViewPresenter: HostedViewPresenter<dydxProfileBalancesViewModel>, dydxProfileBalancesViewPresenterProtocol {
+    override init() {
+        super.init()
+
+        viewModel = dydxProfileBalancesViewModel()
+        viewModel?.nativeTokenName = dydxTokenConstants.nativeTokenName
+        viewModel?.nativeTokenLogoUrl = dydxTokenConstants.nativeTokenLogoUrl
+    }
+
+    public override func start() {
+        super.start()
+
+        AbacusStateManager.shared.state.accountBalance(of: .dydx)
+            .sink { [weak self] dydxAmount in
+                if let dydxAmount = dydxAmount {
+                    self?.viewModel?.walletAmount = dydxFormatter.shared.raw(number: Parser.standard.asNumber(dydxAmount), digits: 4)
+                    self?.viewModel?.transferAction = {
+                        Router.shared?.navigate(to: RoutingRequest(path: "/transfer", params: ["section": "transferOut"]), animated: true, completion: nil)
+                    }
+                } else {
+                    self?.viewModel?.walletAmount = "-"
+                    self?.viewModel?.transferAction = nil
+                }
+            }
+            .store(in: &subscriptions)
+
+        AbacusStateManager.shared.state.stakingBalance(of: .dydx)
+            .sink { [weak self] dydxAmount in
+                if let dydxAmount = dydxAmount {
+                    self?.viewModel?.stakedAmount = dydxFormatter.shared.raw(number: Parser.standard.asNumber(dydxAmount), digits: 4)
+                } else {
+                    self?.viewModel?.stakedAmount = "-"
+                }
+            }
+            .store(in: &subscriptions)
+    }
+}
