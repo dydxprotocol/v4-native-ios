f8412cf63d07ead44a14a7a1a390233eb44fcda5
mike-dydx <mike@dydx.exchange>
2023-12-07 18:20:20 -0500

    add more periods

--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsHistoryViewPresenter.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsHistoryViewPresenter.swift
@@ -59,7 +59,25 @@ public class dydxRewardsHistoryViewPresenter: HostedViewPresenter<dydxRewardsHis
             .init(period: "period 8 -> period 9", amount: "8.000"),
             .init(period: "period 9 -> period 10", amount: "9.000"),
             .init(period: "period 10 -> period 11", amount: "10.000"),
-            .init(period: "period 11 -> period 12", amount: "11.000")
+            .init(period: "period 11 -> period 12", amount: "11.000"),
+            .init(period: "period 12 -> period 2", amount: "1.000"),
+            .init(period: "period 13 -> period 3", amount: "2.000"),
+            .init(period: "period 14 -> period 4", amount: "3.000"),
+            .init(period: "period 15 -> period 5", amount: "4.000"),
+            .init(period: "period 16 -> period 6", amount: "5.000"),
+            .init(period: "period 17 -> period 7", amount: "6.000"),
+            .init(period: "period 18 -> period 8", amount: "7.000"),
+            .init(period: "period 19 -> period 9", amount: "8.000"),
+            .init(period: "period 20 -> period 10", amount: "9.000"),
+            .init(period: "period 21 -> period 11", amount: "10.000"),
+            .init(period: "period 22 -> period 3", amount: "2.000"),
+            .init(period: "period 23 -> period 4", amount: "3.000"),
+            .init(period: "period 24 -> period 5", amount: "4.000"),
+            .init(period: "period 25 -> period 6", amount: "5.000"),
+            .init(period: "period 26 -> period 7", amount: "6.000"),
+            .init(period: "period 27 -> period 8", amount: "7.000"),
+            .init(period: "period 28 -> period 9", amount: "8.000"),
+            .init(period: "period 29 -> period 10", amount: "9.000")
         ]
     }
 }
