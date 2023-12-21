c9c0a15836e5fb6a95c259b5ddfc6e0a16568614
mike-dydx <mike@dydx.exchange>
2023-12-21 17:23:52 -0500

    update script to use temp file so that `main` is always checked out

--- scripts/update_client_api.sh
+++ scripts/update_client_api.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-# Checking if the directory exists
-if [ -d "../../v4-clients/v4-client-js" ]; then
-    cd ../../v4-clients/v4-client-js
-else
-    # If the directory doesn't exist, perform git clone and navigate to the cloned directory
-    cd ../..
-    git clone git@github.com:dydxprotocol/v4-clients.git
-    cd v4-clients/v4-client-js
+# Check if the current directory is named 'scripts'
+ORIG_DIR="$PWD"
+CURRENT_DIR=$(basename "$PWD")
+if [ "$CURRENT_DIR" != "scripts" ]; then
+    echo "This script must be run from the directory named 'scripts' in v4-native-ios/scripts"
+    exit 1
+fi
 
-    # If cloning fails, exit the script
-    if [ $? -ne 0 ]; then
-        echo "Failed to clone the git@github.com:dydxprotocol/v4-clients.git repository. Please check your network connection and repository access."
-        exit 1
-    fi
+# Defining a temporary directory for cloning
+TMP_DIR=$(mktemp -d)
+
+# Function to clean up the temporary directory
+cleanup() {
+    echo "Cleaning up..."
+    rm -rf "$TMP_DIR"
+}
+
+# Trap to clean up in case of script exit or interruption
+trap cleanup EXIT
+
+# Cloning into the temporary directory and navigating there
+git clone git@github.com:dydxprotocol/v4-clients.git "$TMP_DIR/v4-clients"
+cd "$TMP_DIR/v4-clients/v4-client-js"
+
+# If cloning fails, exit the script
+if [ $? -ne 0 ]; then
+    echo "Failed to clone the v4-clients repository. Please check your network connection and repository access."
+    exit 1
 fi
 
 # Running npm commands
@@ -21,8 +35,12 @@ npm install
 npm run build
 npm run webpack
 
-# Copying the file to the specified location
-cp __native__/__ios__/v4-native-client.js ../../v4-native-ios/dydx/dydxPresenters/dydxPresenters/_Features/v4-native-client.js
-
-# Navigating to the final directory
-cd ../../v4-native-ios/scripts
+# Check if the target directory for copying exists
+TARGET_DIR="$ORIG_DIR/../dydx/dydxPresenters/dydxPresenters/_Features/"
+echo "$TARGET_DIR"
+if [ -d "$TARGET_DIR" ]; then
+    # Copying the file to the specified location
+    cp __native__/__ios__/v4-native-client.js "$TARGET_DIR/v4-native-client.js"
+else
+    echo "Target directory $TARGET_DIR does not exist. File not copied."
+fi
