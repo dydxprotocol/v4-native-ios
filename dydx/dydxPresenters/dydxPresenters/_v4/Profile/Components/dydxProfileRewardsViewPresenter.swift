fe846cd44affa9e4eee9bfb8599b6ac14f733925
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2024-01-03 13:18:50 -0500

    TRCL-3408 : Trading Rewards hook in real data (#46)
    * read FAQs from abacus
    
    * read profile rewards amount from abacus
    
    * disable allTimeRewardsAmount (for now)
    
    * update logic for displaying last list item separator
    
    * undo pods changes
    
    * swiftlint

--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/Components/dydxProfileRewardsViewPresenter.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/Components/dydxProfileRewardsViewPresenter.swift
@@ -25,11 +25,22 @@ public class dydxProfileRewardsViewPresenter: HostedViewPresenter<dydxProfileRew
         super.init()
 
         viewModel = dydxProfileRewardsViewModel()
-        viewModel?.last7DaysRewardsAmount = "PLACEHOLDER"
-        viewModel?.allTimeRewardsAmount = "PLACEHOLDER"
 
         viewModel?.tapAction = {
             Router.shared?.navigate(to: RoutingRequest(path: "/profile/trading-rewards"), animated: true, completion: nil)
         }
+    }
+
+    public override func start() {
+        super.start()
+
+        AbacusStateManager.shared.state.account
+            .sink { [weak self] account in
+                // allTimeRewardsAmount is commented out because at time of writing, we do not have historical data accurate for "all time"
+                // see thread: https://dydx-team.slack.com/archives/C066T2L1HM4/p1703107669507409
+//                self?.viewModel?.allTimeRewardsAmount = dydxFormatter.shared.format(decimal: account?.tradingRewards?.total?.decimalValue)
+                self?.viewModel?.last7DaysRewardsAmount = dydxFormatter.shared.format(number: account?.tradingRewards?.historical?["WEEKLY"]?.first?.amount)
+            }
+            .store(in: &subscriptions)
     }
 }
--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsSummaryPresenter.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsSummaryPresenter.swift
@@ -8,6 +8,9 @@
 import dydxViews
 import PlatformParticles
 import ParticlesKit
+import Combine
+import dydxStateManager
+import dydxFormatter
 
 public protocol dydxRewardsSummaryPresenterProtocol: HostedViewPresenterProtocol {
     var viewModel: dydxProfileRewardsViewModel? { get }
@@ -18,7 +21,18 @@ public class dydxRewardsSummaryViewPresenter: HostedViewPresenter<dydxProfileRew
         super.init()
 
         viewModel = dydxProfileRewardsViewModel()
-        viewModel?.last7DaysRewardsAmount = "PLACEHOLDER"
-        viewModel?.allTimeRewardsAmount = "PLACEHOLDER"
+    }
+
+    public override func start() {
+        super.start()
+
+        AbacusStateManager.shared.state.account
+            .sink { [weak self] account in
+                // allTimeRewardsAmount is commented out because at time of writing, we do not have historical data accurate for "all time"
+                // see thread: https://dydx-team.slack.com/archives/C066T2L1HM4/p1703107669507409
+//                self?.viewModel?.allTimeRewardsAmount = dydxFormatter.shared.format(decimal: account?.tradingRewards?.total?.decimalValue)
+                self?.viewModel?.last7DaysRewardsAmount = dydxFormatter.shared.format(number: account?.tradingRewards?.historical?["WEEKLY"]?.first?.amount)
+            }
+            .store(in: &subscriptions)
     }
 }
