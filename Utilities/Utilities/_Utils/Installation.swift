a08d6086cac365435a688f664e3df4df9a398c4c
mike-dydx <mike@dydx.exchange>
2024-01-29 17:43:41 -0500

    formatting

--- Utilities/Utilities/_Utils/Installation.swift
+++ Utilities/Utilities/_Utils/Installation.swift
@@ -29,30 +29,30 @@ public class Installation {
     }
     
     public static var isJailBroken: Bool = {
-#if targetEnvironment(simulator)
-        return false
-#else
-        if FileManager.default.fileExists(atPath: "/Applications/Cydia.app")
-            || FileManager.default.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib")
-            || FileManager.default.fileExists(atPath: "/bin/bash")
-            || FileManager.default.fileExists(atPath: "/usr/sbin/sshd")
-            || FileManager.default.fileExists(atPath: "/etc/apt")
-            || FileManager.default.fileExists(atPath: "/private/var/lib/apt/")
-            || URLHandler.shared?.canOpenURL(URL(string: "cydia://package/com.example.package")!) ?? false {
-            return true
-        }
-        
-        let stringToWrite = "Something to test"
-        let file = "/private/poikjkt.txt"
-        try? FileManager.default.removeItem(atPath: file)
-        do {
-            try stringToWrite.write(toFile: file, atomically: true, encoding: String.Encoding.utf8)
-            
-            return true
-        } catch {
+        #if targetEnvironment(simulator)
             return false
-        }
-#endif
+        #else
+            if FileManager.default.fileExists(atPath: "/Applications/Cydia.app")
+                || FileManager.default.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib")
+                || FileManager.default.fileExists(atPath: "/bin/bash")
+                || FileManager.default.fileExists(atPath: "/usr/sbin/sshd")
+                || FileManager.default.fileExists(atPath: "/etc/apt")
+                || FileManager.default.fileExists(atPath: "/private/var/lib/apt/")
+                || URLHandler.shared?.canOpenURL(URL(string: "cydia://package/com.example.package")!) ?? false {
+                return true
+            }
+
+            let stringToWrite = "Something to test"
+            let file = "/private/poikjkt.txt"
+            try? FileManager.default.removeItem(atPath: file)
+            do {
+                try stringToWrite.write(toFile: file, atomically: true, encoding: String.Encoding.utf8)
+
+                return true
+            } catch {
+                return false
+            }
+        #endif
     }()
     
     public static var source: Source {
@@ -68,10 +68,10 @@ public class Installation {
     }
     
     public static var isSimulator: Bool = {
-#if targetEnvironment(simulator)
-        return true
-#else
-        return false
-#endif
+        #if targetEnvironment(simulator)
+            return true
+        #else
+            return false
+        #endif
     }()
 }
