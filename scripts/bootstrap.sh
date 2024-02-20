b39dba3d4accb13b809d046908799a04b66a6c27
Rui <102453770+ruixhuang@users.noreply.github.com>
2024-02-20 06:47:42 -0800

    Add v4-web and template installation to bootstrap.sh (#88)

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -23,12 +23,23 @@ xcode-kotlin install
 
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
+# Install templates
+./install_templates.sh
+
 cd "$ROOT_DIR"
 
 if [ ! -d "v4-localization" ]; then
 	git clone git@github.com:dydxprotocol/v4-localization.git
 else
 	cd v4-localization
+	git pull
+	cd ..
+fi
+
+if [ ! -d "v4-web" ]; then
+	git clone git@github.com:dydxprotocol/v4-web.git
+else
+	cd v4-web
 	git pull
 	cd ..
 fi
