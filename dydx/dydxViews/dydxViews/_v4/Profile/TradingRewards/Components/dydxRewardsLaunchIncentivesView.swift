07c5998739e8e7d72a0b9142c8931c294713e23d
mike-dydx <mike@dydx.exchange>
2024-02-25 17:28:56 -0500

    remove background image

--- dydx/dydxViews/dydxViews/_v4/Profile/TradingRewards/Components/dydxRewardsLaunchIncentivesView.swift
+++ dydx/dydxViews/dydxViews/_v4/Profile/TradingRewards/Components/dydxRewardsLaunchIncentivesView.swift
@@ -71,11 +71,12 @@ public class dydxRewardsLaunchIncentivesViewModel: PlatformViewModel {
             Image("stars", bundle: .dydxView)
         }
         .padding(.all, 16)
-        .background {
-            Image("texture", bundle: .dydxView)
-                .resizable()
-                .scaledToFill()
-        }
+        // commented out until light and classic mode images are provided
+//        .background {
+//            Image("texture", bundle: .dydxView)
+//                .resizable()
+//                .scaledToFill()
+//        }
         .themeColor(background: .layer4)
         .borderAndClip(style: .cornerRadius(12), borderColor: .layer6, lineWidth: 1)
     }
