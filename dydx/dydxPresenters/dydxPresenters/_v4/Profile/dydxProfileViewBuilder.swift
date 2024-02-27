a7ae0820b86f3838f5639402fc3784e03020c0b7
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2024-02-27 17:36:03 -0500

    MOB-252 : Add link to share app on profile screen (#98)
    * add in-line share action
    
    * use app url instead of website
    
    * add analytics event

--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/dydxProfileViewBuilder.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/dydxProfileViewBuilder.swift
@@ -11,6 +11,7 @@ import PlatformParticles
 import RoutingKit
 import ParticlesKit
 import PlatformUI
+import dydxStateManager
 
 public class dydxProfileViewBuilder: NSObject, ObjectBuilderProtocol {
     public func build<T>() -> T? {
@@ -72,5 +73,13 @@ private class dydxProfileViewPresenter: HostedViewPresenter<dydxProfileViewModel
         balancesPresenter.$viewModel.assign(to: &viewModel.$balances)
 
         attachChildren(workers: childPresenters)
+
+        viewModel.share?.shareAction = {
+            Router.shared?.navigate(to: RoutingRequest(path: "/action/share",
+                                                       params: ["text": DataLocalizer.shared?.localize(path: "APP.GENERAL.SHARE_MESSAGE", params: nil) ?? "",
+                                                                                       "link": AbacusStateManager.shared.environment?.apps?.ios?.url ?? AbacusStateManager.shared.deploymentUri,
+                                                                                       "share_source": "account_screen_share_app_inline_button"]),
+                                                       animated: true, completion: nil)
+        }
     }
 }
