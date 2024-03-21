6bbb1e1eb9396364caeeb7ad50e4fb0dc0792796
mike-dydx <mike@dydx.exchange>
2024-03-21 12:04:35 -0400

    update pods

--- dydx/Pods/Target Support Files/Abacus/Abacus-copy-dsyms.sh
+++ dydx/Pods/Target Support Files/Abacus/Abacus-copy-dsyms.sh
@@ -83,4 +83,4 @@ install_bcsymbolmap() {
     rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}"
 }
 
-install_dsym "${PODS_ROOT}/Abacus/build/cocoapods/framework/Abacus.framework.dSYM"
+install_dsym "${PODS_ROOT}/../../../v4-abacus/build/cocoapods/framework/Abacus.framework.dSYM"
