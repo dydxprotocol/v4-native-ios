1f1e38219f551e9394f53a5d77179cdeebcd0a2d
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2024-03-29 14:33:41 -0400

    MOB-417 : add settings control to turn in-app notifications on/off completely (#124)
    * add new notifications key to settings store which determins if notifications are shown
    
    * fix pre-fill value
    
    * display on/off in settings
    
    * refactor SettingsStore extension

---
+++ dydx/dydxPresenters/dydxPresenters/_v4/KeyValueStoreProtocolStore+Ext.swift
@@ -0,0 +1,53 @@
+//
+//  SettingsStore+Ext.swift
+//  dydxPresenters
+//
+//  Created by Michael Maguire on 3/28/24.
+//
+
+import Foundation
+import Utilities
+import dydxViews
+
+enum dydxSettingsStoreKey: String {
+    case language = "language"
+    case v4Theme = "v4_theme"
+    case directionColorPreference = "direction_color_preference"
+    case shouldDisplayInAppNotifications = "should_display_in_app_notifications"
+
+    var defaultValue: Any? {
+        switch self {
+        case .language: return DataLocalizer.shared?.language
+        case .v4Theme: return dydxThemeType.dark.rawValue
+        case .directionColorPreference: return "green_is_up"
+        case .shouldDisplayInAppNotifications: return true
+        }
+    }
+}
+
+extension KeyValueStoreProtocol {
+
+    var language: Bool {
+        SettingsStore.shared?.value(forKey: dydxSettingsStoreKey.language.rawValue) as? Bool
+        ?? dydxSettingsStoreKey.language.defaultValue as? Bool
+        ?? true
+    }
+
+    var v4Theme: Bool {
+        SettingsStore.shared?.value(forKey: dydxSettingsStoreKey.v4Theme.rawValue) as? Bool
+        ?? dydxSettingsStoreKey.v4Theme.defaultValue as? Bool
+        ?? true
+    }
+
+    var directionColorPreference: Bool {
+        SettingsStore.shared?.value(forKey: dydxSettingsStoreKey.directionColorPreference.rawValue) as? Bool
+        ?? dydxSettingsStoreKey.directionColorPreference.defaultValue as? Bool
+        ?? true
+    }
+
+    var shouldDisplayInAppNotifications: Bool {
+        SettingsStore.shared?.value(forKey: dydxSettingsStoreKey.shouldDisplayInAppNotifications.rawValue) as? Bool
+        ?? dydxSettingsStoreKey.shouldDisplayInAppNotifications.defaultValue as? Bool
+        ?? true
+    }
+}
