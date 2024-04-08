3db65fe7c70a132a181b06bc52d37c7bf96546a0
Rui <102453770+ruixhuang@users.noreply.github.com>
2024-04-08 16:36:52 -0700

    Update v4-native-client.js (#136)

--- dydx/dydxStateManager/dydxStateManager/Protocol_Implementations/AbacusChainImp.swift
+++ dydx/dydxStateManager/dydxStateManager/Protocol_Implementations/AbacusChainImp.swift
@@ -12,19 +12,25 @@ import Utilities
 final public class AbacusChainImp: Abacus.DYDXChainTransactionsProtocol {
     public func connectNetwork(paramsInJson: String, callback: @escaping (String?) -> Void) {
         CosmoJavascript.shared.connectNetwork(paramsInJson: paramsInJson) { result in
-            callback(result as? String)
+            let result = result as? String
+            Console.shared.log("AbacusChainImp.connectNetwork \(paramsInJson), result: \(result ?? "nil")")
+            callback(result)
         }
     }
 
     public func transaction(type: TransactionType, paramsInJson: String?, callback: @escaping (String?) -> Void) {
         CosmoJavascript.shared.call(functionName: type.rawValue, paramsInJson: paramsInJson) { result in
-            callback(result as? String)
+            let result = result as? String
+            Console.shared.log("AbacusChainImp.transaction \(type.rawValue), param \(paramsInJson ?? "nil"), result: \(result ?? "nil")")
+            callback(result)
         }
     }
 
     public func get(type: QueryType, paramsInJson: String?, callback: @escaping (String?) -> Void) {
         CosmoJavascript.shared.call(functionName: type.rawValue, paramsInJson: paramsInJson) { result in
-            callback(result as? String)
+            let result = result as? String
+            Console.shared.log("AbacusChainImp.get \(type.rawValue), param \(paramsInJson ?? "nil"), result: \(result ?? "nil")")
+            callback(result)
         }
     }
 
