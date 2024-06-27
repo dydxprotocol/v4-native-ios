76167c2f1fa629122f505f7eb0a4a96d7b7748d6
mike-dydx <mike@dydx.exchange>
2024-06-27 16:30:05 -0400

    incl 3x as a target leverage

--- dydx/dydxPresenters/dydxPresenters/_v4/Trade/Margin/dydxTargetLeverageViewBuilder.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Trade/Margin/dydxTargetLeverageViewBuilder.swift
@@ -93,7 +93,7 @@ private class dydxTargetLeverageViewPresenter: HostedViewPresenter<dydxTargetLev
                 guard let viewModel = self?.viewModel, let marketId = tradeInput?.marketId, let market = configsAndAssetMap[marketId] else { return }
                 if let imf = market.configs?.effectiveInitialMarginFraction?.doubleValue, imf > 0 {
                     let maxLeverage = 1.0 / imf
-                    viewModel.leverageOptions = [1, 2, 5, 10]
+                    viewModel.leverageOptions = [1, 2, 3, 5, 10]
                         .filter { $0 < maxLeverage }
                         .map { dydxTargetLeverageViewModel.LeverageTextAndValue(text: dydxFormatter.shared.multiplier(number: Double($0)) ?? "", value: $0) }
                     viewModel.leverageOptions.append(dydxTargetLeverageViewModel.LeverageTextAndValue(text: DataLocalizer.localize(path: "APP.GENERAL.MAX"), value: maxLeverage))
