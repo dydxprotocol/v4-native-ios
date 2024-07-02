3500ff015e5c3e4ec6b92be78d5f0e404584a94b
mike-dydx <mike@dydx.exchange>
2024-07-02 10:51:45 -0400

    cache displayed alerts to not display again

--- dydx/dydxPresenters/dydxPresenters/_v4/GlobalWorkers/Workers/dydxAlertsWorker.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/GlobalWorkers/Workers/dydxAlertsWorker.swift
@@ -32,14 +32,30 @@ extension Abacus.NotificationType {
 }
 
 final class dydxAlertsWorker: BaseWorker {
-    private var handledAlertHashes = Set<Int>()
+    static let userDefaultsKey: String = "dydxAlertsWorker"
+
+    // do not set directly, use `markAlertAsHandled` instead
+    private var handledAlertIds = {
+        if let handledAlertIds = UserDefaults.standard.array(forKey: dydxAlertsWorker.userDefaultsKey) as? [String] {
+            return Set(handledAlertIds)
+        } else {
+            return Set<String>()
+        }
+    }()
+
+    private func markAlertAsHandled(_ alert: Abacus.Notification) {
+        if handledAlertIds.insert(alert.betterId).inserted {
+            var handledAlerts = Array(handledAlertIds)
+            handledAlerts.append(alert.betterId)
+            UserDefaults.standard.set(handledAlerts, forKey: dydxAlertsWorker.userDefaultsKey)
+        }
+    }
 
     override func start() {
         super.start()
 
         AbacusStateManager.shared.state.alerts
             .removeDuplicates()
-            .receive(on: RunLoop.main)
             .sink { [weak self] alerts in
                 self?.updateAlerts(alerts: alerts)
             }
@@ -49,7 +65,7 @@ final class dydxAlertsWorker: BaseWorker {
     private func updateAlerts(alerts: [Abacus.Notification]) {
         alerts
             // don't display an alert which has already been handled
-            .filter { !handledAlertHashes.contains($0.hashValue) }
+            .filter { !handledAlertIds.contains($0.betterId) }
             // display alerts in chronological order they were received
             .sorted { $0.updateTimeInMilliseconds < $1.updateTimeInMilliseconds }
             .forEach { alert in
@@ -58,13 +74,22 @@ final class dydxAlertsWorker: BaseWorker {
                     Router.shared?.navigate(to: RoutingRequest(path: link!), animated: true, completion: nil)
                 }] : nil
                 if SettingsStore.shared?.shouldDisplayInAppNotifications != false {
-                    ErrorInfo.shared?.info(title: alert.title,
-                                           message: alert.text,
-                                           type: alert.type.infoType,
-                                           error: nil, time: nil, actions: actions)
+                    DispatchQueue.main.async {
+                        ErrorInfo.shared?.info(title: alert.title,
+                                               message: alert.text,
+                                               type: alert.type.infoType,
+                                               error: nil, time: nil, actions: actions)
+                    }
                 }
                 // add to alert ids set to avoid double handling
-                handledAlertHashes.insert(alert.hashValue)
+                markAlertAsHandled(alert)
             }
+    }
+}
+
+private extension Abacus.Notification {
+    // id is just the fill id which is not guaranteed unique
+    var betterId: String {
+        "\(self.id)\(self.updateTimeInMilliseconds)\(self.title)"
     }
 }
