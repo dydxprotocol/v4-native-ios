d0d9a44e20fd8b40d033d7d7b57bdd65aa33bc4f
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2024-07-24 20:29:36 -0400

    add slider to target leverage screen (#213)
    * create slider text input view
    
    * add target leverage slider
    
    * temp
    
    * fix custom amount switch behavior and fix overwriting bug
    
    * undo changes to platforminput
    
    * fix lines to 1 for leverage buttons
    
    * temp
    
    * fix precision
    
    * add precision to slider
    
    * clean up
    
    * address PR comments
    
    ---------
    
    Co-authored-by: Mike <mike-dydx@users.noreply.github.com>

---
+++ dydx/dydxViews/dydxViews/Shared/dydxComponents/dydxTitledNumberField.swift
@@ -0,0 +1,143 @@
+//
+//  dydxNumberFieldView.swift
+//  dydxViews
+//
+//  Created by Michael Maguire on 7/18/24.
+//
+
+import SwiftUI
+import dydxFormatter
+import PlatformUI
+
+/// Effectively a TextField which forces its input as a number
+/// Supports dydx-style title and title accesory view
+struct dydxTitledNumberField: View {
+    let title: String?
+    let accessoryTitle: String?
+    let numberFormatter: dydxNumberInputFormatter
+    let minValue: Double
+    let maxValue: Double
+    @Binding var value: Double?
+    @State private var textWidth: CGFloat = 0
+
+    let minWidth: CGFloat = 100
+
+    @ViewBuilder
+    private var accessoryTitleView: some View {
+        if let text = accessoryTitle {
+            TokenTextViewModel(symbol: text)
+                .createView(parentStyle: .defaultStyle.themeFont(fontType: .base, fontSize: .smallest))
+        }
+    }
+
+    @ViewBuilder
+    private var titleView: some View {
+        if let text = title {
+            Text(text)
+                .themeColor(foreground: .textTertiary)
+                .themeFont(fontType: .base, fontSize: .smaller)
+                .background(
+                    // this ensures that the text does not grow wider than the title or min width
+                    GeometryReader { geometry in
+                        Color.clear
+                            .onAppear {
+                                textWidth = max(geometry.size.width, minWidth)
+                            }
+                    }
+                )
+                .fixedSize()
+        }
+    }
+
+    private var textFieldView: some View {
+        NumberTextField(
+            actualValue: $value,
+            minValue: minValue,
+            maxValue: maxValue,
+            numberFormatter: numberFormatter)
+        .themeColor(foreground: .textPrimary)
+        .themeFont(fontType: .base, fontSize: .medium)
+        .truncationMode(.middle)
+        .frame(width: textWidth)
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            HStack(spacing: 5) {
+                titleView
+                accessoryTitleView
+            }
+            textFieldView
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 12)
+        .makeInput()
+    }
+}
+
+/// formats input as it is received
+private struct NumberTextField: View {
+    @Binding var actualValue: Double?
+    @FocusState private var isFocused: Bool
+
+    let minValue: Double
+    let maxValue: Double
+    let numberFormatter: dydxNumberInputFormatter
+
+    private var keyboardType: UIKeyboardType {
+        numberFormatter.fractionDigits > 0 ? .decimalPad : .numberPad
+    }
+
+    @ViewBuilder
+    private var placeholder: some View {
+        Text(numberFormatter.string(for: Double.zero) ?? "")
+            .themeFont(fontType: .base, fontSize: .medium)
+            .themeColor(foreground: .textTertiary)
+    }
+
+    var body: some View {
+        TextField(text: filteredTextBinding) {
+            placeholder
+        }
+        .keyboardType(keyboardType)
+        .focused($isFocused)
+        .onChange(of: isFocused) { _ in
+            if !isFocused, let value = Double(filteredTextBinding.wrappedValue) {
+                actualValue = formatValue(value)
+            }
+        }
+        .keyboardAccessory(parentStyle: .defaultStyle)
+    }
+
+    private func formatValue(_ value: Double?) -> Double? {
+        guard let value = value else {
+            return nil
+        }
+        let multiplier = pow(10.0, Double(numberFormatter.fractionDigits))
+        let formattedValue = (value * multiplier).rounded() / multiplier
+        return formattedValue
+    }
+
+    private var filteredTextBinding: Binding<String> {
+        Binding<String>(
+            get: {
+                if let value = actualValue {
+                    return numberFormatter.string(from: NSNumber(value: value)) ?? ""
+                } else {
+                    return ""
+                }
+            },
+            set: { newValue in
+                if let doubleValue = Double(newValue) {
+                    actualValue = formatValue(clamp(doubleValue))
+                } else {
+                    actualValue = nil
+                }
+            }
+        )
+    }
+
+    private func clamp(_ value: Double) -> Double {
+        min(max(value, minValue), maxValue)
+    }
+}
