72997868e75dd58f9be56dbea4e468f49022925e
Mike <mike-dydx@users.noreply.github.com>
2024-07-29 18:57:13 -0400

    implement default value and session feature flag consistency

--- StatsigInjections/StatsigInjections/StatsigInjections.swift
+++ StatsigInjections/StatsigInjections/StatsigInjections.swift
@@ -13,6 +13,14 @@ import Combine
 
     private let apiKey: String
     private let environment: StatsigEnvironment
+    // ensures feature flag values stay constant throughout the app session after they are used the first time, even if they are initialized to null
+    private var sessionValues = [String: Availabilty]()
+    
+    private enum Availabilty {
+        case available(Bool)
+        // unavailable is the case for a new feature flag, or a first launch of the app with Statsig
+        case unavailable
+    }
     
     public enum Environment {
         case production
@@ -67,7 +75,6 @@ import Combine
                     if let error {
                         Console.shared.log("Statsig feature flags failed to initialize: \(error)")
                     }
-                    self?.newValuesSubject.send()
                     // intentionally not calling completion here since we do not want ff init to be blocking startup
                     // this may change if we need FF pre-launch
 //                    completion()
@@ -75,14 +82,30 @@ import Combine
         }
         completion()
     }
-    
-    private let newValuesSubject = PassthroughSubject<Void, Never>()
-    public var newValuesAvailablePublisher: AnyPublisher<Void, Never> {
-        newValuesSubject.eraseToAnyPublisher()
-    }
 
+    // https://docs.statsig.com/sdk/debugging
     public func isOn(feature: String) -> Bool? {
-        Statsig.checkGate(feature)
+        let featureGate = Statsig.getFeatureGate(feature)
+        let availability: Availabilty
+        if let existingAvailability = sessionValues[feature] {
+            // a featuregate value has already been set for this session
+            availability = existingAvailability
+        } else if featureGate.evaluationDetails.reason == .Recognized {
+            // a featuregate will be recognized if the feature gate has been fetched in previous Statsig inits (cache is non-empty)
+            availability = .available(featureGate.value)
+        } else {
+            // a featuregate will be unrecognized if the feature gate is new or this is first time initializing Statsig (cache would be empty)
+            availability = .unavailable
+        }
+        sessionValues[feature] = availability
+        
+        switch availability {
+        case .available(let bool):
+            return bool
+        case .unavailable:
+            // defer to calling context for default value
+            return nil
+        }
     }
     
     public func value(feature: String) -> String? {
