5d22c1ebc10db4eb62c396f8f9eca8ecd7ff5c61
Mike <mike-dydx@users.noreply.github.com>
2024-08-01 17:51:50 -0400

    add apr-tvl row

--- dydx/dydxViews/dydxViews/_v4/Vault/Landing/dydxVaultViewModel.swift
+++ dydx/dydxViews/dydxViews/_v4/Vault/Landing/dydxVaultViewModel.swift
@@ -37,13 +37,22 @@ private struct dydxVaultView: View {
             Spacer().frame(height: 28)
             vaultPnlRow
             Spacer().frame(height: 16)
-            Rectangle()
-                .themeColor(foreground: .borderDefault)
-                .frame(height: 1)
-            Spacer(minLength: 16)
+            div
+            Spacer().frame(height: 16)
+            aprTvlRow
+            Spacer().frame(height: 16)
+            div
+            Spacer()
+
         }
         .frame(maxWidth: .infinity)
         .themeColor(background: .layer2)
+    }
+
+    var div: some View {
+        Rectangle()
+            .themeColor(foreground: .borderDefault)
+            .frame(height: 1)
     }
 
     // MARK: - Header
@@ -82,6 +91,7 @@ private struct dydxVaultView: View {
         return (Text(DataLocalizer.shared?.localize(path: "APP.GENERAL.LEARN_MORE", params: nil) ?? "") + Text(" ") + Text(image))
             .themeColor(foreground: .textSecondary)
             .themeFont(fontType: .base, fontSize: .medium)
+            .padding(.trailing, 12)
     }
 
     // MARK: - Section 1
@@ -98,15 +108,14 @@ private struct dydxVaultView: View {
         VStack(spacing: 4) {
             Text(DataLocalizer.shared?.localize(path: "APP.VAULTS.YOUR_VAULT_BALANCE", params: nil) ?? "")
                 .themeColor(foreground: .textTertiary)
-                .themeFont(fontType: .base, fontSize: .smaller)
+                .themeFont(fontType: .base, fontSize: .small)
             Text(dydxFormatter.shared.dollar(number: viewModel.vaultBalance) ?? "--")
                 .themeColor(foreground: .textPrimary)
                 .themeFont(fontType: .base, fontSize: .medium)
         }
+        .leftAligned()
         .padding(.horizontal, 16)
         .padding(.vertical, 12)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-
         .borderAndClip(style: .cornerRadius(10), borderColor: .borderDefault)
     }
 
@@ -114,7 +123,7 @@ private struct dydxVaultView: View {
         VStack(spacing: 4) {
             Text(DataLocalizer.shared?.localize(path: "APP.VAULTS.YOUR_ALL_TIME_PNL", params: nil) ?? "")
                 .themeColor(foreground: .textTertiary)
-                .themeFont(fontType: .base, fontSize: .smaller)
+                .themeFont(fontType: .base, fontSize: .small)
             VStack {
                 VStack(spacing: 2) {
                     SignedAmountViewModel(text: dydxFormatter.shared.dollar(number: viewModel.profitDollars) ?? "--", sign: .none, coloringOption: .allText)
@@ -127,10 +136,44 @@ private struct dydxVaultView: View {
                 }
             }
         }
+        .leftAligned()
         .padding(.horizontal, 16)
         .padding(.vertical, 12)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .borderAndClip(style: .cornerRadius(10), borderColor: .borderDefault)
     }
+
+    // MARK: - Section 2
+    var aprTvlRow: some View {
+        HStack(spacing: 32) {
+            aprTitleValue
+            tvlTitleValue
+        }
+        .leftAligned()
+        .padding(.horizontal, 16)
+    }
+
+    var aprTitleValue: some View {
+        VStack(spacing: 4) {
+            Text(DataLocalizer.shared?.localize(path: "APP.VAULTS.VAULT_THIRTY_DAY_APR", params: nil) ?? "")
+                .themeColor(foreground: .textTertiary)
+                .themeFont(fontType: .base, fontSize: .small)
+            Text("--")
+                .themeColor(foreground: .textPrimary)
+                .themeFont(fontType: .base, fontSize: .medium)
+        }
+    }
+
+    var tvlTitleValue: some View {
+        VStack(spacing: 4) {
+            Text(DataLocalizer.shared?.localize(path: "APP.VAULTS.TVL", params: nil) ?? "")
+                .themeColor(foreground: .textTertiary)
+                .themeFont(fontType: .base, fontSize: .small)
+            Text("--")
+                .themeColor(foreground: .textPrimary)
+                .themeFont(fontType: .base, fontSize: .medium)
+        }
+    }
+
+    // MARK: - Section 3 - graph
 
 }
