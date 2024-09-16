1d63576428d741820b4acaba0bf55816445f8b88
mike-dydx <149746839+mike-dydx@users.noreply.github.com>
2024-09-16 13:27:56 -0400

    confirm screen states (#248)

--- dydx/dydxPresenters/dydxPresenters/_v4/Vault/DepositsAndWithdrawals/dydxVaultDepositWithdrawViewBuilder.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Vault/DepositsAndWithdrawals/dydxVaultDepositWithdrawViewBuilder.swift
@@ -15,6 +15,9 @@ import dydxStateManager
 import FloatingPanel
 import PlatformRouting
 import dydxFormatter
+import Combine
+// import single class due to VaultTransferType collision
+import class Abacus.Subaccount
 
 public class dydxVaultDepositWithdrawViewBuilder: NSObject, ObjectBuilderProtocol {
     public func build<T>() -> T? {
@@ -30,10 +33,10 @@ private class dydxVaultDepositWithdrawViewController: HostingViewController<Plat
     override public func arrive(to request: RoutingRequest?, animated: Bool) -> Bool {
         let presenter = presenter as? dydxVaultDepositWithdrawViewPresenterProtocol
         if request?.path == "/vault/deposit" {
-            presenter?.transferType = .deposit
+            presenter?.viewModel?.selectedTransferType = .deposit
             return true
         } else if request?.path == "/vault/withdraw" {
-            presenter?.transferType = .withdraw
+            presenter?.viewModel?.selectedTransferType = .withdraw
             return true
         } else {
             return false
@@ -43,62 +46,127 @@ private class dydxVaultDepositWithdrawViewController: HostingViewController<Plat
 
 private protocol dydxVaultDepositWithdrawViewPresenterProtocol: HostedViewPresenterProtocol {
     var viewModel: dydxVaultDepositWithdrawViewModel? { get }
-    var transferType: VaultTransferType { get set }
 }
 
 private class dydxVaultDepositWithdrawViewPresenter: HostedViewPresenter<dydxVaultDepositWithdrawViewModel>, dydxVaultDepositWithdrawViewPresenterProtocol {
-    var transferType: VaultTransferType = .deposit
-
     override init() {
         super.init()
 
-        let viewModel = dydxVaultDepositWithdrawViewModel(selectedTransferType: transferType, submitState: .enabled)
-        viewModel.submitAction = { [weak self] in
-            guard let selectedTransferType = self?.viewModel?.selectedTransferType else { return }
-            switch selectedTransferType {
-            case .deposit:
-                Router.shared?.navigate(to: RoutingRequest(path: "/vault/deposit_confirm"), animated: true, completion: nil)
-            case .withdraw:
-                Router.shared?.navigate(to: RoutingRequest(path: "/vault/withdraw_confirm"), animated: true, completion: nil)
-            }
-        }
+        let viewModel = dydxVaultDepositWithdrawViewModel()
 
         self.viewModel = viewModel
     }
 
     override func start() {
         super.start()
-
-        // TODO: replace with real hooks from abacus
-        update()
+        guard let viewModel = viewModel else { return }
+        
+        Publishers.CombineLatest3(AbacusStateManager.shared.state.selectedSubaccount, viewModel.$amount, viewModel.$selectedTransferType)
+            .sink { [weak self] selectedSubaccount, amount, transferType in
+                self?.update(subaccount: selectedSubaccount, amount: amount ?? 0, transferType: transferType)
+            }
+            .store(in: &subscriptions)
     }
 
     // TODO: replace with real data from abacus
-    func update() {
-        var newInputReceiptChangeItems = [dydxReceiptChangeItemView]()
-        var newButtonReceiptChangeItems = [dydxReceiptChangeItemView]()
+    func update(subaccount: Subaccount?, amount: Double, transferType: VaultTransferType) {
+        viewModel?.maxAmount = subaccount?.freeCollateral?.current?.doubleValue ?? 0
 
-        newInputReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.YOUR_VAULT_BALANCE"),
-                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
-                                                                      after: AmountTextModel(amount: 30.02))))
-
-        newButtonReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.GENERAL.CROSS_FREE_COLLATERAL"),
-                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
-                                                                      after: AmountTextModel(amount: 30.02))))
-
-        newButtonReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.EST_SLIPPAGE"),
-                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
-                                                                      after: AmountTextModel(amount: 30.02))))
-
-        newButtonReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.WITHDRAW_MODAL.EXPECTED_AMOUNT_RECEIVED"),
-                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
-                                                                      after: AmountTextModel(amount: 30.02))))
-
-        viewModel?.inputReceiptChangeItems = newInputReceiptChangeItems
-        viewModel?.buttonReceiptChangeItems = newButtonReceiptChangeItems
+        updateSubmitState(amount: amount)
+        updateReceiptItems(subaccount: subaccount, amount: amount, transferType: transferType)
+        updateSubmitAction(amount: amount, transferType: transferType)
 
         viewModel?.inputInlineAlert = InlineAlertViewModel(InlineAlertViewModel.Config.init(title: "test alert",
                                                                                             body: "test bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest bodytest body",
                                                                                             level: .error))
+    }
+    
+    private func updateSubmitState(amount: Double) {
+        if amount > 0 {
+            viewModel?.submitState = .enabled
+        } else {
+            viewModel?.submitState = .disabled
+        }
+    }
+    
+    private func updateReceiptItems(subaccount: Subaccount?, amount: Double, transferType: VaultTransferType) {
+        guard let curVaultBalance = Optional(420.0),
+              let curFreeCollateral = subaccount?.freeCollateral?.current?.doubleValue,
+              let curMarginUsage = subaccount?.marginUsage?.current?.doubleValue
+        else { return }
+        
+        var newInputReceiptChangeItems = [dydxReceiptChangeItemView]()
+        var newButtonReceiptChangeItems = [dydxReceiptChangeItemView]()
+
+        let preTransferVaultBalance = AmountTextModel(amount: curVaultBalance.asNsNumber)
+        let preTransferFreeCollateral = AmountTextModel(amount: curFreeCollateral.asNsNumber)
+        let preTransferMarginUsage = AmountTextModel(amount: curMarginUsage.asNsNumber)
+        
+        let postTransferVaultBalance: AmountTextModel?
+        let postTransferFreeCollateral: AmountTextModel?
+        let postTransferMarginUsage: AmountTextModel?
+        
+        if amount > 0 {
+            switch transferType {
+            case .deposit:
+                postTransferVaultBalance = AmountTextModel(amount: (curVaultBalance + amount).asNsNumber)
+                postTransferFreeCollateral = AmountTextModel(amount: (curFreeCollateral - amount).asNsNumber)
+                //TODO: this is wrong calculation
+                postTransferMarginUsage = AmountTextModel(amount: (curMarginUsage - amount).asNsNumber)
+            case .withdraw:
+                postTransferVaultBalance = AmountTextModel(amount: (curVaultBalance - amount).asNsNumber)
+                postTransferFreeCollateral = AmountTextModel(amount: (curFreeCollateral + amount).asNsNumber)
+                //TODO: this is wrong calculation
+                postTransferMarginUsage = AmountTextModel(amount: (curMarginUsage + amount).asNsNumber)
+            }
+        } else {
+            postTransferVaultBalance = nil
+            postTransferFreeCollateral = nil
+            postTransferMarginUsage = nil
+        }
+        
+        let vaultBalanceReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.YOUR_VAULT_BALANCE"),
+                                                                    value: AmountChangeModel(before: preTransferVaultBalance, after: postTransferVaultBalance))
+        let freeCollateralReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.GENERAL.FREE_COLLATERAL"),
+                                                                    value: AmountChangeModel(before: preTransferFreeCollateral, after: postTransferFreeCollateral))
+        let marginUsageReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.GENERAL.MARGIN_USAGE"),
+                                                                    value: AmountChangeModel(before: preTransferMarginUsage, after: postTransferMarginUsage))
+        
+        switch transferType {
+        case .deposit:
+            newInputReceiptChangeItems.append(freeCollateralReceiptItem)
+            newButtonReceiptChangeItems.append(marginUsageReceiptItem)
+            newButtonReceiptChangeItems.append(vaultBalanceReceiptItem)
+        case .withdraw:
+            newInputReceiptChangeItems.append(vaultBalanceReceiptItem)
+            newButtonReceiptChangeItems.append(freeCollateralReceiptItem)
+            newButtonReceiptChangeItems.append(marginUsageReceiptItem)
+        }
+
+//        newButtonReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.EST_SLIPPAGE"),
+//                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
+//                                                                      after: AmountTextModel(amount: 30.02))))
+
+//        newButtonReceiptChangeItems.append(dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.WITHDRAW_MODAL.EXPECTED_AMOUNT_RECEIVED"),
+//                                                         value: AmountChangeModel(before: AmountTextModel(amount: 30.01),
+//                                                                      after: AmountTextModel(amount: 30.02))))
+
+        viewModel?.inputReceiptChangeItems = newInputReceiptChangeItems
+        viewModel?.buttonReceiptChangeItems = newButtonReceiptChangeItems
+    }
+        
+    private func updateSubmitAction(amount: Double, transferType: VaultTransferType) {
+        viewModel?.submitAction = {
+            Router.shared?.navigate(to: RoutingRequest(path: transferType.confirmScreenPath, params: ["amount": amount]), animated: true, completion: nil)
+        }
+    }
+}
+
+private extension VaultTransferType {    
+    var confirmScreenPath: String {
+        switch self {
+        case .deposit: return "/vault/deposit_confirm"
+        case .withdraw: return "/vault/withdraw_confirm"
+        }
     }
 }
--- dydx/dydxViews/dydxViews/_v4/Vault/DepositAndWithdrawal/dydxVaultDepositWithdrawConfirmationViewModel.swift
+++ dydx/dydxViews/dydxViews/_v4/Vault/DepositAndWithdrawal/dydxVaultDepositWithdrawConfirmationViewModel.swift
@@ -13,29 +13,36 @@ import dydxFormatter
 public class dydxVaultDepositWithdrawConfirmationViewModel: PlatformViewModel {
     public enum State {
         case enabled
+        case submitting
+        case loading
         case disabled
     }
+    
+    @Published public var amount: Double?
+    @Published public var transferType: VaultTransferType?
+    @Published public var faqUrl: String?
 
-    fileprivate var submitState: State {
-        hasAcknowledgedHighSlippage ? .enabled : .disabled
+    @Published public var submitState: State = .enabled
+
+    @Published public var submitAction: (() -> Void)?
+    @Published public var cancelAction: (() -> Void)?
+    @Published public var curVaultBalance: Double?
+    @Published public var curFreeCollateral: Double?
+    @Published public var curMarginUsage: Double?
+    @Published public var postVaultBalance: Double?
+    @Published public var postFreeCollateral: Double?
+    @Published public var postMarginUsage: Double?
+    @Published public var slippage: Double?
+    public var expectedAmount: Double? {
+        guard let slippage = slippage, let amount = amount else { return nil }
+        return amount * slippage
     }
 
-    public var submitAction: (() -> Void)?
-    public var cancelAction: (() -> Void)?
-    public var elevatedSlippageAmount: Double?
-    public var receiptItems: [dydxReceiptChangeItemView]?
-
-    @Published public var transferType: VaultTransferType
     @Published public var requiresAcknowledgeHighSlippage: Bool = false
+    @Published public var isFirstSubmission: Bool = true
 
-    @Published fileprivate var amount: Double?
-    @Published fileprivate var hasAcknowledgedHighSlippage: Bool = false
-
-    public init(transferType: VaultTransferType) {
-        self.transferType = transferType
-
-    }
-
+    @Published fileprivate(set) public var hasAcknowledgedHighSlippage: Bool = false
+    
     public override func createView(parentStyle: ThemeStyle = ThemeStyle.defaultStyle, styleKey: String? = nil) -> PlatformView {
         PlatformView(viewModel: self, parentStyle: parentStyle, styleKey: styleKey) { [weak self] _ in
             guard let self = self else { return AnyView(PlatformView.nilView) }
@@ -79,10 +86,13 @@ private struct VaultDepositWithdrawConfirmationView: View {
         .padding(.horizontal, 8)
     }
 
+    @ViewBuilder
     private var titleText: some View {
-        Text(viewModel.transferType.confirmTransferText)
-            .themeColor(foreground: .textPrimary)
-            .themeFont(fontType: .plus, fontSize: .largest)
+        if let transferType = viewModel.transferType {
+            Text(transferType.confirmTransferText)
+                .themeColor(foreground: .textPrimary)
+                .themeFont(fontType: .plus, fontSize: .largest)
+        }
     }
 
     private var backButton: some View {
@@ -90,17 +100,20 @@ private struct VaultDepositWithdrawConfirmationView: View {
             .createView()
     }
 
+    @ViewBuilder
     private var transferVisualization: some View {
-        return HStack(alignment: .center, spacing: 16) {
-            generationTransferStep(title: viewModel.transferType.transferOriginTitleText, thumbnail: viewModel.transferType.transferOriginImage, subtitle: "placeholder")
-            Image(systemName: "chevron.forward.dotted.chevron.forward")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 16, height: 16)
-                .themeColor(foreground: .textTertiary)
-            generationTransferStep(title: viewModel.transferType.transferDestinationTitleText, thumbnail: viewModel.transferType.transferDestinationImage, subtitle: viewModel.transferType.transferDestinationSubtitleText)
+        if let transferType = viewModel.transferType {
+            HStack(alignment: .center, spacing: 16) {
+                generationTransferStep(title: transferType.transferOriginTitleText, thumbnail: transferType.transferOriginImage, subtitle: dydxFormatter.shared.dollar(number: viewModel.amount)  ?? "--")
+                Image(systemName: "chevron.forward.dotted.chevron.forward")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                    .themeColor(foreground: .textTertiary)
+                generationTransferStep(title: transferType.transferDestinationTitleText, thumbnail: transferType.transferDestinationImage, subtitle: transferType.transferDestinationSubtitleText)
+            }
+            .fixedSize(horizontal: false, vertical: true)
         }
-        .fixedSize(horizontal: false, vertical: true)
 
     }
 
@@ -140,9 +153,49 @@ private struct VaultDepositWithdrawConfirmationView: View {
 
     @ViewBuilder
     private var receipts: some View {
-        if viewModel.receiptItems?.count ?? 0 > 0 {
+        if let transferType = viewModel.transferType {
+            let slippageTitle = DataLocalizer.localize(path: "APP.VAULTS.EST_SLIPPAGE")
+            let expectedAmountTitle = DataLocalizer.localize(path: "APP.WITHDRAW_MODAL.EXPECTED_AMOUNT_RECEIVED")
+            
+            let preTransferVaultBalance = AmountTextModel(amount: viewModel.curVaultBalance?.asNsNumber)
+            let preTransferFreeCollateral = AmountTextModel(amount: viewModel.curFreeCollateral?.asNsNumber)
+            let preTransferMarginUsage = AmountTextModel(amount: viewModel.curMarginUsage?.asNsNumber)
+            
+            let postTransferVaultBalance = AmountTextModel(amount: viewModel.postVaultBalance?.asNsNumber)
+            let postTransferFreeCollateral = AmountTextModel(amount: viewModel.postFreeCollateral?.asNsNumber)
+            let postTransferMarginUsage = AmountTextModel(amount: viewModel.postMarginUsage?.asNsNumber)
+            
+            let estSlippage = AmountTextModel(amount: viewModel.slippage?.asNsNumber, unit: .percentage)
+            let expectedAmount = AmountTextModel(amount: viewModel.expectedAmount?.asNsNumber)
+            
+            let vaultBalanceReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.YOUR_VAULT_BALANCE"),
+                                                                        value: AmountChangeModel(before: preTransferVaultBalance, after: postTransferVaultBalance))
+            let freeCollateralReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.GENERAL.FREE_COLLATERAL"),
+                                                                        value: AmountChangeModel(before: preTransferFreeCollateral, after: postTransferFreeCollateral))
+            let marginUsageReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.GENERAL.MARGIN_USAGE"),
+                                                                        value: AmountChangeModel(before: preTransferMarginUsage, after: postTransferMarginUsage))
+            let estSlippageReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.VAULTS.EST_SLIPPAGE"),
+                                                                        value: AmountChangeModel(before: estSlippage, after: nil))
+            let expectedAmountReceiptItem = dydxReceiptChangeItemView(title: DataLocalizer.localize(path: "APP.WITHDRAW_MODAL.EXPECTED_AMOUNT_RECEIVED"),
+                                                                        value: AmountChangeModel(before: expectedAmount, after: nil))
+
             VStack(spacing: 8) {
-                ForEach(viewModel.receiptItems ?? [], id: \.id) { $0.createView() }
+                switch transferType {
+                case .deposit:
+                    freeCollateralReceiptItem.createView()
+                    marginUsageReceiptItem.createView()
+                    vaultBalanceReceiptItem.createView()
+                case .withdraw:
+                    freeCollateralReceiptItem.createView()
+                    vaultBalanceReceiptItem.createView()
+                    if let slippage = viewModel.slippage {
+                        estSlippageReceiptItem.createView()
+                        expectedAmountReceiptItem.createView()
+                    } else {
+                        dydxReceiptLoadingItemView(title: slippageTitle).createView()
+                        dydxReceiptLoadingItemView(title: expectedAmountTitle).createView()
+                    }
+                }
             }
             .padding(.all, 16)
             .themeColor(background: .layer2)
@@ -152,13 +205,12 @@ private struct VaultDepositWithdrawConfirmationView: View {
 
     @ViewBuilder
     private var withdrawalSlippageInlineAlert: some View {
-        if let elevatedSlippageAmount = viewModel.elevatedSlippageAmount {
+        if let faqUrl = viewModel.faqUrl,
+            let slippage = viewModel.slippage, viewModel.requiresAcknowledgeHighSlippage {
             let withdrawalSlippageInlineAlertAttributedText = AttributedString(localizerPathKey: "APP.VAULTS.SLIPPAGE_WARNING",
                                                                                params: ["LINK": DataLocalizer.localize(path: "APP.VAULTS.VAULT_FAQS"),
-                                                                                        "AMOUNT": dydxFormatter.shared.percent(number: elevatedSlippageAmount, digits: 2) ?? "--"],
-                                                                               // TODO: Replace
-                                                                               hyperlinks: ["withdraw": "https://test.com",
-                                                                                            "APP.VAULTS.VAULT_FAQS": "https://purple.com"],
+                                                                                        "AMOUNT": dydxFormatter.shared.percent(number: slippage, digits: 2) ?? "--"],
+                                                                               hyperlinks: ["APP.VAULTS.VAULT_FAQS": faqUrl],
                                                                                foreground: .textPrimary)
             InlineAlertViewModel(.init(title: nil,
                                        body: withdrawalSlippageInlineAlertAttributedText,
@@ -172,26 +224,45 @@ private struct VaultDepositWithdrawConfirmationView: View {
         if viewModel.requiresAcknowledgeHighSlippage {
             dydxCheckboxView(isChecked: $viewModel.hasAcknowledgedHighSlippage,
                              text: DataLocalizer.localize(path: "APP.VAULTS.SLIPPAGE_ACK",
-                                                          params: ["AMOUNT": dydxFormatter.shared.percent(number: viewModel.elevatedSlippageAmount, digits: 2) ?? "--"]))
+                                                          params: ["AMOUNT": dydxFormatter.shared.percent(number: viewModel.slippage, digits: 2) ?? "--"]))
+            .disabled(viewModel.submitState == .submitting)
+        }
+    }
+    
+    @ViewBuilder
+    private var spinner: some View {
+        ProgressView()
+            .progressViewStyle(.circular)
+            .tint(ThemeColor.SemanticColor.textSecondary.color)
+    }
+    
+    @ViewBuilder
+    public var buttonContent: some View {
+        if let transferType = viewModel.transferType {
+            Group {
+                switch viewModel.submitState {
+                case .enabled, .loading:
+                    Text(viewModel.isFirstSubmission ? transferType.confirmTransferText : DataLocalizer.localize(path: "APP.ONBOARDING.TRY_AGAIN"))
+                case .submitting:
+                    HStack {
+                        Text(DataLocalizer.localize(path: "APP.TRADE.SUBMITTING"))
+                        spinner
+                    }
+                case .disabled:
+                    if viewModel.requiresAcknowledgeHighSlippage && !viewModel.hasAcknowledgedHighSlippage {
+                        Text(DataLocalizer.localize(path: "APP.VAULTS.ACKNOWLEDGE_HIGH_SLIPPAGE"))
+                    } else {
+                        Text(transferType.confirmTransferText)
+                    }
+                }
+            }
+            .themeFont(fontType: .base, fontSize: .large)
         }
     }
 
     private var submitButton: some View {
-        let content: Text
-        let state: PlatformButtonState
-        switch viewModel.submitState {
-        case .enabled:
-            state = .primary
-            content = Text(viewModel.transferType.confirmTransferText)
-                .themeColor(foreground: .textPrimary)
-                .themeFont(fontType: .base, fontSize: .large)
-        case .disabled:
-            state = .disabled
-            content = Text(DataLocalizer.localize(path: "APP.VAULTS.ACKNOWLEDGE_HIGH_SLIPPAGE"))
-                .themeColor(foreground: .textTertiary)
-                .themeFont(fontType: .base, fontSize: .large)
-        }
-        return PlatformButtonViewModel(content: content.wrappedViewModel, state: state, action: viewModel.submitAction ?? {})
+        let state: PlatformButtonState = viewModel.submitState == .enabled ? .primary : .disabled
+        return PlatformButtonViewModel(content: buttonContent.wrappedViewModel, state: state, action: viewModel.submitAction ?? {})
             .createView()
     }
 }
