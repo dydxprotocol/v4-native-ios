20a96cad1d9a6deaa83c7dd386c1569b2bd339c3
Rui <rui@dydx.exchange>
2024-09-17 11:29:53 -0700

    Clean up

--- dydx/dydxPresenters/dydxPresenters/_v4/KeyValueStoreProtocolStore+Ext.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/KeyValueStoreProtocolStore+Ext.swift
@@ -22,9 +22,9 @@ public enum dydxSettingsStoreKey: String, CaseIterable {
         case .language: return DataLocalizer.shared?.language
         case .v4Theme: return dydxThemeType.classicDark.rawValue
         case .directionColorPreference: return "green_is_up"
-        case .shouldDisplayInAppNotifications: return false
+        case .shouldDisplayInAppNotifications: return true
         case .gasToken: return "USDC"
-        case .hidePredictionMarketsNoticeKey: return true
+        case .hidePredictionMarketsNoticeKey: return false
         }
     }
 }
