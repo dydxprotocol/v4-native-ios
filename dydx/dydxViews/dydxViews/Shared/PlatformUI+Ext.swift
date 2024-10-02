c595a570a068e9b2499986fac3391008f0cff618
Mike <mike-dydx@users.noreply.github.com>
2024-10-01 20:54:20 -0400

    convenience builder for url icons and placeholders

---
+++ dydx/dydxViews/dydxViews/Shared/PlatformUI+Ext.swift
@@ -0,0 +1,26 @@
+//
+//  PlatformUI+Ext.swift
+//  dydxViews
+//
+//  Created by Michael Maguire on 10/1/24.
+//
+
+import PlatformUI
+import SwiftUI
+
+extension PlatformIconViewModel {
+    public convenience init(url: URL?, placeholderText: String?) {
+        if let placeholderText {
+            let placeholderContent = { Text(placeholderText)
+                    .frame(width: 32, height: 32)
+                    .themeColor(foreground: .textTertiary)
+                    .themeColor(background: .layer5)
+                    .borderAndClip(style: .circle, borderColor: .layer7, lineWidth: 1)
+                    .wrappedInAnyView()
+            }
+            self.init(type: .url(url: url, placeholderContent: placeholderContent))
+        } else {
+            self.init(type: .url(url: url, placeholderContent: nil))
+        }
+    }
+}
