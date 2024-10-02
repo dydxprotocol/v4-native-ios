7bbe2bdd2f80a66b0a126918ef6263346e648587
Prashan Dharmasena <prashan@dydx.exchange>
2024-10-02 17:49:14 -0400

    Add withdrawal wallet rebalancing and increase withdrawal threshold.

--- dydx/dydxPresenters/dydxPresenters/_v4/GlobalWorkers/Workers/dydxTransferSubaccountWorker.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/GlobalWorkers/Workers/dydxTransferSubaccountWorker.swift
@@ -15,7 +15,8 @@ import dydxFormatter
 
 final class dydxTransferSubaccountWorker: BaseWorker {
 
-    private static let balanceRetainAmount = 0.5
+    private static let balanceRetainAmount = 1.25
+    private static let rebalanceThreshold = 1.0
 
     override func start() {
         super.start()
@@ -27,6 +28,25 @@ final class dydxTransferSubaccountWorker: BaseWorker {
             .sink { [weak self] balance, walletState in
                 guard let balance, balance > dydxTransferSubaccountWorker.balanceRetainAmount else { return }
                 let depositAmount = balance - dydxTransferSubaccountWorker.balanceRetainAmount
+                let amountString = dydxFormatter.shared.decimalLocaleAgnostic(number: NSNumber(value: depositAmount),
+                                                                              digits: dydxTokenConstants.usdcTokenDecimal)
+                if let amountString = amountString {
+                    self?.depositToSubaccount(amount: amountString,
+                                              subaccount: AbacusStateManager.shared.selectedSubaccountNumber,
+                                              walletState: walletState)
+                } else {
+                    Console.shared.log("dydxTransferSubaccountWorker: Invalid amount")
+                }
+            }
+            .store(in: &subscriptions)
+
+        AbacusStateManager.shared.state.accountBalance(of: AbacusStateManager.shared.environment?.usdcTokenInfo?.denom)
+            .withLatestFrom(
+                AbacusStateManager.shared.state.walletState
+            )
+            .sink { [weak self] balance, walletState in
+                guard let balance, balance < dydxTransferSubaccountWorker.rebalanceThreshold else { return }
+                let depositAmount = dydxTransferSubaccountWorker.rebalanceThreshold - balance
                 let amountString = dydxFormatter.shared.decimalLocaleAgnostic(number: NSNumber(value: depositAmount),
                                                                               digits: dydxTokenConstants.usdcTokenDecimal)
                 if let amountString = amountString {
@@ -56,6 +76,26 @@ final class dydxTransferSubaccountWorker: BaseWorker {
                     trackingData["error"] = resultString
                 }
                 Tracking.shared?.log(event: "SubaccountDeposit_Failed", data: trackingData)
+            }
+        }
+    }
+
+    private func withdrawToSubaccount(amount: String, subaccount: Int, walletState: dydxWalletState) {
+        CosmoJavascript.shared.withdrawToSubaccount(subaccount: subaccount, amount: amount) { result in
+            var trackingData = [
+                "amount": "\(amount)",
+                "address": "\(String(describing: walletState.currentWallet?.cosmoAddress))"
+            ]
+            if let result = (result as? String)?.jsonDictionary,
+               result["code"] as? Int == 0,
+               result["hash"] != nil {
+                Tracking.shared?.log(event: "SubaccountWithdraw", data: trackingData)
+            } else {
+                Console.shared.log("Withdraw to subaccount failed")
+                if let resultString = result as? String {
+                    trackingData["error"] = resultString
+                }
+                Tracking.shared?.log(event: "SubaccountWithdraw_Failed", data: trackingData)
             }
         }
     }
