e7bef4f3d7519e06ff14eec59235a5bf6bc20c3d
Mike <mike-dydx@users.noreply.github.com>
2024-10-16 16:40:36 -0400

    Revert "use EU for amplitude server location (#275)"
    This reverts commit e5a92dd47606a594f7a2e119e4c4998aab0abd94.

--- AmplitudeInjections/AmplitudeInjections/_Tracking/AmplitudeTracking.swift
+++ AmplitudeInjections/AmplitudeInjections/_Tracking/AmplitudeTracking.swift
@@ -14,7 +14,7 @@ open class AmplitudeTracking: TransformerTracker {
     private let amplitude: Amplitude
 
     public init(_ apiKey: String) {
-        self.amplitude = Amplitude.init(configuration: .init(apiKey: apiKey, serverZone: .EU))
+        self.amplitude = Amplitude.init(configuration: .init(apiKey: apiKey))
         Console.shared.log("analytics log | Amplitude initialized")
         super.init()
     }
