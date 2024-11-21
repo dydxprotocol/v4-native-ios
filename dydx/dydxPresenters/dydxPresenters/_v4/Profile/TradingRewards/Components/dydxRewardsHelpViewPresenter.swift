bd1a5570f0d0f956c3f20474fa52965341c15c05
Rui <rui@dydx.exchange>
2024-11-21 14:51:38 -0800

    BUG2-31 Fix rewards learn more

--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsHelpViewPresenter.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/TradingRewards/Components/dydxRewardsHelpViewPresenter.swift
@@ -10,6 +10,8 @@ import PlatformParticles
 import ParticlesKit
 import RoutingKit
 import dydxStateManager
+import Abacus
+import Utilities
 
 public protocol dydxRewardsHelpViewPresenterProtocol: HostedViewPresenterProtocol {
     var viewModel: dydxRewardsHelpViewModel? { get }
@@ -22,8 +24,12 @@ public class dydxRewardsHelpViewPresenter: HostedViewPresenter<dydxRewardsHelpVi
         viewModel = dydxRewardsHelpViewModel()
 
         viewModel?.learnMoreTapped = {
-            // TODO get url from abacus
-//            Router.shared?.navigate(to: .init(url: ...), animated: true, completion: nil)
+            if let urlString = AbacusStateManager.shared.environment?.links?.tradingRewardsLearnMore,
+               let url = URL(string: urlString) {
+                if URLHandler.shared?.canOpenURL(url) ?? false {
+                    URLHandler.shared?.open(url, completionHandler: nil)
+                }
+            }
         }
 
         let faqs = AbacusStateManager.shared.documentation?.tradingRewardsFAQs.map { faq in
