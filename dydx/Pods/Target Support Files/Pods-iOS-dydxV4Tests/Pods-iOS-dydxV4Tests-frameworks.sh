72967354bdfbe0590691b35e31198f37806319de
Rui <rui@dydx.exchange>
2024-12-13 14:33:59 -0500

    Fix pod issue

--- dydx/Pods/Target Support Files/Pods-iOS-dydxV4Tests/Pods-iOS-dydxV4Tests-frameworks.sh
+++ dydx/Pods/Target Support Files/Pods-iOS-dydxV4Tests/Pods-iOS-dydxV4Tests-frameworks.sh
@@ -228,7 +228,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AnalyticsConnector/AnalyticsConnector.framework"
-  install_framework "${PODS_ROOT}/../../../v4-abacus/build/cocoapods/framework/Abacus.framework"
+  install_framework "${PODS_ROOT}/Abacus/build/cocoapods/framework/Abacus.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CombineExt/CombineExt.framework"
@@ -283,7 +283,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AmplitudeSwift/AmplitudeSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AnalyticsConnector/AnalyticsConnector.framework"
-  install_framework "${PODS_ROOT}/../../../v4-abacus/build/cocoapods/framework/Abacus.framework"
+  install_framework "${PODS_ROOT}/Abacus/build/cocoapods/framework/Abacus.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
