2c805744b1cf1f26be680fbde548c4aafbd62825
Rui <rui@dydx.exchange>
2025-07-22 20:37:22 -0700

    Got Google token

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import {AppRegistry, NativeModules, DeviceEventEmitter} from 'react-native';
 import App from './App';
-import 'react-native-get-random-values'
+import 'react-native-get-random-values';
 import {TurnkeyLogin} from './TurnkeyReact/TurnkeyLogin';
+import 'react-native-url-polyfill/auto';
 
 AppRegistry.registerComponent('TurnkeyReact', () => App);
 AppRegistry.registerComponent('TurnkeyLogin', () => TurnkeyLogin);
